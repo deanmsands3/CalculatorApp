6f40fcb897e54187b39d6c709777609d9d9b295f
Dean McGinty Sands, III <deanmsands3@gmail.com>
2017-08-03 12:38:51 -0500

    Cleaning up the Part2 code.

--- main.cpp
+++ main.cpp
@@ -21,12 +21,12 @@ bool is_number(const std::string& s)
     return true;
 }
 
-char to_op(const string &op){
+char to_op(const string& op){
 	return op.c_str()[0];
 }
 
 
-bool is_operator(string op){
+bool is_operator(const string& op){
 	if(op.length()>1){return false;}
 	char c_op=to_op(op);
 
@@ -49,7 +49,7 @@ bool is_operator(string op){
 
 }
 
-double operate(double left, double right, char op){
+double operate(const double left, const double right, const char op){
 	switch(op){
 		case '+':
 			return left+right;
@@ -71,13 +71,13 @@ double operate(double left, double right, char op){
 
 int main(int argc, char *args[]){
 	//Variables
-	stack<double> values;
 	string token_string;
 	//Read in operation
 	while(std::getline(std::cin, token_string)){
 		//Convert to stream
 		istringstream token_stream(token_string);
 		string token;
+		stack<double> values;
 		//Empty stack if not empty (should be empty)
 		while(!values.empty()){values.pop();}
 		//Read tokens into stack
@@ -92,20 +92,23 @@ int main(int argc, char *args[]){
 				char c_op=to_op(token);
 				//Is there enough to work with?
 				if(values.size()<2){return -1;} //No, there isn't.
-				//Operate on the values
+				//Grab values from stack, then operate on them
 				double right=values.top();values.pop();
 				double left=values.top();values.pop();
 				double product=operate(left, right, c_op);
 				values.push(product);
 			}
-
+			
+			//Neither/nor. I can't work with this.
 			else {cout<<"Invalid token received:"<<token<<endl; return -1;}
-
 		}
+
 		//Do we have only one value left?
 		if(values.size()!=1) {cout<<"Too many/few values left on stack."<<endl;return -1;}
 		//Print it out
 		cout<<"Result:"<<values.top()<<endl;
+		//Pop last value from stack		
+		values.pop();
 	}
 	return 0;
 }
